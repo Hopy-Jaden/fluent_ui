e2ed5d9ce8c3e8695d9589b5fab1de2bd06d81f8
Hopy-Jaden <96371992+Hopy-Jaden@users.noreply.github.com>
2025-12-23 15:00:13 +0800

    Settings Card Page Creation I

--- example/lib/screens/design_pattern/settings_card.dart
+++ example/lib/screens/design_pattern/settings_card.dart
@@ -10,11 +10,10 @@ class SettingsCardPage extends StatefulWidget {
   State<SettingsCardPage> createState() => _SettingsCardPageState();
 }
 
-class _SettingsCardPageState extends State<SettingsCardPage>
-    with PageMixin {
+class _SettingsCardPageState extends State<SettingsCardPage> with PageMixin {
   static const double itemHeight = 500;
-    bool selected = true;
-      String? comboboxValue;
+  bool selected = true;
+  String? comboboxValue;
 
   int topIndex = 0;
 
@@ -29,102 +28,257 @@ class _SettingsCardPageState extends State<SettingsCardPage>
   @override
   Widget build(final BuildContext context) {
     return ScaffoldPage.scrollable(
-      header: const PageHeader(title: Text('Create your first app')),
+      header: const PageHeader(title: Text('Settings Card')),
       children: [
-        
         const Text(
-          'Follow these steps to create your first Fluent UI app using the FluentApp() and ScaffoldPage() widgets:',
+          'A card control that can be used to create Windows 11 style settings experience, introduced in the windows community toolkit gallery. '
+          'Follow these steps to create Settings Card using existing widget provided from this library.',
         ),
         const SizedBox(height: 30),
-        Text('Installation', style: FluentTheme.of(context).typography.subtitle,),
-        const SizedBox(height: 10,),
-        Text( 
-          'Before you begin, ensure you have Flutter installed on your machine. Then, add the '
-          'fluent_ui package to your project by including it in your pubspec.yaml file:',
+        Text(
+          'Simple Setting Card',
+          style: FluentTheme.of(context).typography.title,
+        ),
+        const SizedBox(height: 10),
+        const Text(
+          'SettingsCard is a control that can be used to display settings in your experience. '
+          'It uses the default styling found in Windows 11 and is easy to use, meets all accessibility standards '
+          'and will make your settings page look great! It typically contains a header, description, header icon and content to create an easy '
+          'to use experience',
+        ),
+        const SizedBox(height: 30),
+        Text(
+          'Step 1: Wrap the ListTile() widget with a Card() widget',
+          style: FluentTheme.of(context).typography.subtitle,
+        ),
+        const SizedBox(height: 10),
+        const Text(
+          'Set the vertical padding of the list tile in the card widget as 8.0.',
         ),
         const SizedBox(height: 20),
         CardHighlight(
           initiallyOpen: false,
           codeSnippet: '''
-dependencies:
-  fluent_ui: ^4.13.0''',
+Card(
+  padding: EdgeInsetsGeometry.symmetric(vertical: 8.0),
+  child: ListTile()
+),
+''',
+          child: const Card(
+            padding: EdgeInsetsGeometry.symmetric(vertical: 8.0),
+            child: ListTile(),
+          ),
         ),
         const SizedBox(height: 30),
-        Text('Step 1: Create a FluentApp() widget', style: FluentTheme.of(context).typography.subtitle,),
-        const SizedBox(height: 10,),
         Text(
-          'The FluentApp widget is the root of your application. It provides '
-          'theming and navigation capabilities for Fluent UI apps.',
+          'Step 2: Put Widgets inside ListTile() based on your need',
+          style: FluentTheme.of(context).typography.subtitle,
+        ),
+        const SizedBox(height: 10),
+        const Text(
+          'Some settings card do not have header icon while some do not have description.',
         ),
         const SizedBox(height: 20),
         CardHighlight(
           initiallyOpen: true,
           codeSnippet: '''
-import 'package:fluent_ui/fluent_ui.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return FluentApp(
-      title: 'Windows UI for Flutter',
-      theme: FluentThemeData(
-        brightness: Brightness.light,
-        accentColor: Colors.blue,
-      ),
-      home: const HomePage(), //which is usually a ScaffoldPage() or NavigationView()
-    );
-  }
-}
-''', 
+Card(
+  padding: EdgeInsetsGeometry.symmetric(vertical: 8.0),
+  child: ListTile(
+    title: Text('Header'), //Header of the settings
+    subtitle: Text('description'), //Description of the settings
+    leading: Icon(WindowsIcons.action_center), //Icon typically included in the card
+    trailing: Button(
+      child: Text('Settings'), 
+      onPressed: () {}
+    ), //Any widget to modify the settings
+  ),
+),
+''',
+          child: Card(
+            padding: EdgeInsetsGeometry.symmetric(vertical: 8.0),
+            child: ListTile(
+              title: Text('Header'),
+              subtitle: Text('description'),
+              leading: Icon(WindowsIcons.action_center),
+              trailing: Button(child: Text('Settings'), onPressed: () {}),
+            ),
+          ),
         ),
-        const SizedBox(height: 30,),
-        Text('Step 2: Set ScaffoldPage() widget as the home of FluentApp()', style: FluentTheme.of(context).typography.subtitle,),
-        const SizedBox(height: 10,),
+        const SizedBox(height: 60),
+        Text(
+          'Clickable Setting Card',
+          style: FluentTheme.of(context).typography.title,
+        ),
+        const SizedBox(height: 10),
         const Text(
-          'The ScaffoldPage widget provides a basic structure for your app\'s '
-          'UI, including a header and content area.',
+          'SettingsCard can also be turned into a button. This can be useful whenever you want your settings '
+          'component to navigate to a detail page or open an external link. ',
+        ),
+        const SizedBox(height: 30),
+        Text(
+          'Step 1: Begin with a HoverButton() returning FocusBorder()',
+          style: FluentTheme.of(context).typography.subtitle,
+        ),
+        const SizedBox(height: 10),
+        Text(
+          'SettingsCard can also be turned into a button. This can be useful whenever you want your settings '
+          'component to navigate to a detail page or open an external link. ',
         ),
         const SizedBox(height: 20),
         CardHighlight(
           initiallyOpen: false,
           codeSnippet: '''
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const ScaffoldPage(
-      header: PageHeader(title: Text('My First Fluent App'),),
-      content: Center(child: Text('Welcome to your first Fluent UI app!')), //or any widget you like
-    );
+HoverButton(
+  onPressed: () {
+    //function when the settings card is clicked
+  },
+  builder: (final context, final states) {
+    return FocusBorder();
   }
-}
+),
 ''',
-          child: SizedBox(height: itemHeight, child: FluentApp(
-      title: 'Windows UI for Flutter',
-      theme: FluentThemeData(
-        brightness: Brightness.light,
-        accentColor: Colors.blue,
-      ),
-      home:  const ScaffoldPage(
-        header: PageHeader(title: Text('My First Fluent App'),),
-        content: Center(child: Text('Welcome to my first Fluent UI app!')), //or any widget you like
-      ),
-    ))
         ),
-        const SizedBox(height: 30,),
-        Text('Step 3: Continue your app development!', style: FluentTheme.of(context).typography.subtitle,),
-        const SizedBox(height: 10,),
+        const SizedBox(height: 30),
+
         Text(
-          'Now that you have set up the basic structure of your Fluent UI app, '
-          'you can start adding more features and functionality to it. Explore other '
-          'pages to learn more about the available widgets and how to use them in your app.',
+          'Step 2: Set AnimatedContainer() as the child',
+          style: FluentTheme.of(context).typography.subtitle,
+        ),
+        const SizedBox(height: 10),
+        Text(
+          'SettingsCard can also be turned into a button. This can be useful whenever you want your settings '
+          'component to navigate to a detail page or open an external link. ',
         ),
         const SizedBox(height: 20),
+        CardHighlight(
+          initiallyOpen: false,
+          codeSnippet: '''
+HoverButton(
+  onPressed: () {
+    //function when the settings card is clicked
+  },
+  builder: (final context, final states) {
+    return FocusBorder(
+      focused: states.isFocused,
+      renderOutside: false,
+      child: AnimatedContainer(),
+    );
+  }
+),
+''',
+        ),
+        const SizedBox(height: 30),
+        Text(
+          'Step 3: Decorate AnimatedContainer()',
+          style: FluentTheme.of(context).typography.subtitle,
+        ),
+        const SizedBox(height: 10),
+        Text(
+          'SettingsCard can also be turned into a button. This can be useful whenever you want your settings '
+          'component to navigate to a detail page or open an external link. ',
+        ),
+        const SizedBox(height: 20),
+        CardHighlight(
+          initiallyOpen: true,
+          codeSnippet: '''
+HoverButton(
+  onPressed: () {
+    //function when the settings card is clicked
+  },
+  builder: (final context, final states) {
+    return FocusBorder(
+      focused: states.isFocused,
+      renderOutside: false,
+      child: AnimatedContainer(
+        duration: FluentTheme.of(context).fasterAnimationDuration,
+        decoration: BoxDecoration(
+          color: ButtonThemeData.uncheckedInputColor(
+            FluentTheme.of(context),
+            states,
+            transparentWhenNone: true,
+          ),
+        ),
+        child: Card(), //Introduce the settings card
+      ),
+    );
+  }
+),
+''',
+        ),
+        const SizedBox(height: 30),
+        Text(
+          'Step 4: Create the Settings Card as the child of AnimatedContainer()',
+          style: FluentTheme.of(context).typography.subtitle,
+        ),
+        const SizedBox(height: 10),
+        Text(
+          'SettingsCard can also be turned into a button. This can be useful whenever you want your settings '
+          'component to navigate to a detail page or open an external link. ',
+        ),
+        const SizedBox(height: 20),
+        CardHighlight(
+          codeSnippet: '''
+HoverButton(
+  onPressed: () {
+    //function when the settings card is clicked
+  },
+  builder: (final context, final states) {
+    return FocusBorder(
+      focused: states.isFocused,
+      renderOutside: false,
+      child: AnimatedContainer(
+        duration: FluentTheme.of(context).fasterAnimationDuration,
+        decoration: BoxDecoration(
+          color: ButtonThemeData.uncheckedInputColor(
+            FluentTheme.of(context),
+            states,
+            transparentWhenNone: true,
+          ),
+        ),
+        child:  Card(
+          padding: EdgeInsetsGeometry.symmetric(vertical: 8.0),
+          child: ListTile(
+            title: Text('Theme'),
+            subtitle: Text('Change the appearance of the app'),
+            leading: Icon(WindowsIcons.color),
+            trailing: Icon(WindowsIcons.chevron_right),
+          ),
+        ),
+      ),
+    );
+  }
+),
+''',
+          child: HoverButton(
+            onPressed: () {},
+            builder: (final context, final states) {
+              return FocusBorder(
+                focused: states.isFocused,
+                renderOutside: false,
+                child: AnimatedContainer(
+                  duration: FluentTheme.of(context).fasterAnimationDuration,
+                  decoration: BoxDecoration(
+                    color: ButtonThemeData.uncheckedInputColor(
+                      FluentTheme.of(context),
+                      states,
+                      transparentWhenNone: true,
+                    ),
+                  ),
+                  child: Card(
+                    padding: EdgeInsetsGeometry.symmetric(vertical: 8.0),
+                    child: ListTile(
+                      title: Text('Theme'),
+                      subtitle: Text('Change the appearance of the app'),
+                      leading: Icon(WindowsIcons.color),
+                      trailing: Icon(WindowsIcons.chevron_right),
+                    ),
+                  ), 
+                ),
+              );
+            },
+          ),
+        ),
       ],
     );
   }
